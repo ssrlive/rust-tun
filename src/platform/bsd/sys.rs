e568b50f5703907d9ecd4eb0e769b8dd3cd4d9af
xmh0511 <970252187@qq.com>
2024-02-29 13:55:19 +0800

    update

--- src/platform/bsd/sys.rs
+++ src/platform/bsd/sys.rs
@@ -12,26 +12,49 @@
 //
 //  0. You just DO WHAT THE FUCK YOU WANT TO.
 
-//! Bindings to internal Linux stuff.
+//! Bindings to internal FreeBSD stuff.
 
 use libc::{c_int, ifreq};
 use nix::{ioctl_read_bad, ioctl_write_ptr, ioctl_write_ptr_bad};
 
-ioctl_read_bad!(siocgifflags, 0x8913, ifreq);
-ioctl_write_ptr_bad!(siocsifflags, 0x8914, ifreq);
-ioctl_read_bad!(siocgifaddr, 0x8915, ifreq);
-ioctl_write_ptr_bad!(siocsifaddr, 0x8916, ifreq);
-ioctl_read_bad!(siocgifdstaddr, 0x8917, ifreq);
-ioctl_write_ptr_bad!(siocsifdstaddr, 0x8918, ifreq);
-ioctl_read_bad!(siocgifbrdaddr, 0x8919, ifreq);
-ioctl_write_ptr_bad!(siocsifbrdaddr, 0x891a, ifreq);
-ioctl_read_bad!(siocgifnetmask, 0x891b, ifreq);
-ioctl_write_ptr_bad!(siocsifnetmask, 0x891c, ifreq);
-ioctl_read_bad!(siocgifmtu, 0x8921, ifreq);
-ioctl_write_ptr_bad!(siocsifmtu, 0x8922, ifreq);
-ioctl_write_ptr_bad!(siocsifname, 0x8923, ifreq);
+#[allow(non_camel_case_types)]
+#[repr(C)]
+#[derive(Copy, Clone)]
+pub struct ctl_info {
+    pub ctl_id: c_uint,
+    pub ctl_name: [c_char; 96],
+}
 
-ioctl_write_ptr!(tunsetiff, b'T', 202, c_int);
-ioctl_write_ptr!(tunsetpersist, b'T', 203, c_int);
-ioctl_write_ptr!(tunsetowner, b'T', 204, c_int);
-ioctl_write_ptr!(tunsetgroup, b'T', 206, c_int);
+#[allow(non_camel_case_types)]
+#[repr(C)]
+#[derive(Copy, Clone)]
+pub struct ifaliasreq {
+    pub ifran: [c_char; IFNAMSIZ],
+    pub addr: sockaddr,
+    pub broadaddr: sockaddr,
+    pub mask: sockaddr,
+}
+
+ioctl_write_ptr!(tunsetiff, b'i', 17, c_int);
+
+
+ioctl_write_ptr!(siocsifflags, b'i', 16, ifreq);
+ioctl_readwrite!(siocgifflags, b'i', 17, ifreq);
+
+ioctl_write_ptr!(siocsifaddr, b'i', 12, ifreq);
+ioctl_readwrite!(siocgifaddr, b'i', 33, ifreq);
+
+ioctl_write_ptr!(siocsifdstaddr, b'i', 14, ifreq);
+ioctl_readwrite!(siocgifdstaddr, b'i', 34, ifreq);
+
+ioctl_write_ptr!(siocsifbrdaddr, b'i', 19, ifreq);
+ioctl_readwrite!(siocgifbrdaddr, b'i', 35, ifreq);
+
+ioctl_write_ptr!(siocsifnetmask, b'i', 22, ifreq);
+ioctl_readwrite!(siocgifnetmask, b'i', 37, ifreq);
+
+ioctl_write_ptr!(siocsifmtu, b'i', 52, ifreq);
+ioctl_readwrite!(siocgifmtu, b'i', 51, ifreq);
+
+ioctl_write_ptr!(siocaifaddr, b'i', 43, ifaliasreq);
+ioctl_write_ptr!(siocdifaddr, b'i', 25, ifreq);
