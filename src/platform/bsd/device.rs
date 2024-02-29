5dcdd3239dfbc6698688becf2a4eb6a525072027
xmh0511 <970252187@qq.com>
2024-02-29 11:15:02 +0800

    update

--- src/platform/bsd/device.rs
+++ src/platform/bsd/device.rs
@@ -92,6 +92,7 @@ impl Device {
 
             let tun = {
                 let fd = libc::open(b"/dev/net/tun\0".as_ptr() as *const _, O_RDWR);
+				dbg!("invoke here");
                 let tun = Fd::new(fd).map_err(|_| io::Error::last_os_error())?;
 
                 if let Err(err) = tunsetiff(tun.0, &mut req as *mut _ as *mut _) {
