69cf35606713f9df16921aef3fcebf7d5823d17d
xmh0511 <970252187@qq.com>
2024-02-29 17:44:32 +0800

    update

--- src/platform/bsd/device.rs
+++ src/platform/bsd/device.rs
@@ -100,6 +100,7 @@ impl Device {
 				let device = format!("/dev/{dev_name}\0");
                 let fd = libc::open(device.as_ptr() as *const _, O_RDWR);
                 let tun = Fd::new(fd).map_err(|_| io::Error::last_os_error())?;
+				println!("{tun:?}");
 				println!("{:?}",req);
                 if let Err(err) = siocgifflags(tun.0, &mut req as *mut _ as *mut _) {
                     dbg!("error in 96",err);
