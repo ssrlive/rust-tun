f63a04d73dbbaf3774d1cfebfb1695685e9a8058
xmh0511 <970252187@qq.com>
2024-03-01 16:49:36 +0800

    set tun name

--- examples/dev-config.rs
+++ examples/dev-config.rs
@@ -63,6 +63,8 @@ fn main_entry(quit: Receiver<()>) -> Result<(), BoxError> {
 
 	dev.set_tun_name("tun3")?;
 
+	dev.set_address(std::net::IpAddr::V4(Ipv4Addr::new(10, 0, 0, 21)))?;
+
     // let r = dev.broadcast()?;
     // println!("{:?}",r);
 
