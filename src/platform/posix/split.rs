0e2c5d7b430480a986848b170d17dc3e63afed8c
xmh0511 <970252187@qq.com>
2024-03-06 10:40:10 +0800

    cargo fmt

--- src/platform/posix/split.rs
+++ src/platform/posix/split.rs
@@ -42,14 +42,14 @@ pub(crate) fn generate_packet_information(
     const TUN_PROTO_IP4: [u8; PIL] = (libc::ETH_P_IP as u32).to_be_bytes();
 
     #[cfg(any(target_os = "macos", target_os = "ios"))]
-    const TUN_PROTO_IP6: [u8; PIL] = (0x000A as u32).to_be_bytes();
+    const TUN_PROTO_IP6: [u8; PIL] = 0x000A_u32.to_be_bytes();
     #[cfg(any(target_os = "macos", target_os = "ios"))]
-    const TUN_PROTO_IP4: [u8; PIL] = (0x0002 as u32).to_be_bytes();
+    const TUN_PROTO_IP4: [u8; PIL] = 0x0002_u32.to_be_bytes();
 
     #[cfg(target_os = "freebsd")]
-    const TUN_PROTO_IP6: [u8; PIL] = (0x86DD as u32).to_be_bytes();
+    const TUN_PROTO_IP6: [u8; PIL] = 0x86DD_u32.to_be_bytes();
     #[cfg(target_os = "freebsd")]
-    const TUN_PROTO_IP4: [u8; PIL] = (0x0800 as u32).to_be_bytes();
+    const TUN_PROTO_IP4: [u8; PIL] = 0x0800_u32.to_be_bytes();
 
     #[cfg(unix)]
     if _packet_information {
