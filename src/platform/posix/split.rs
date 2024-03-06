73812c90948a4b96f2f6fc951eaf92b1014a6da2
xmh0511 <970252187@qq.com>
2024-03-06 10:36:25 +0800

    fix packet-information

--- src/platform/posix/split.rs
+++ src/platform/posix/split.rs
@@ -42,14 +42,14 @@ pub(crate) fn generate_packet_information(
     const TUN_PROTO_IP4: [u8; PIL] = (libc::ETH_P_IP as u32).to_be_bytes();
 
     #[cfg(any(target_os = "macos", target_os = "ios"))]
-    const TUN_PROTO_IP6: [u8; PIL] = (libc::AF_INET6 as u32).to_be_bytes();
+    const TUN_PROTO_IP6: [u8; PIL] = (0x000A as u32).to_be_bytes();
     #[cfg(any(target_os = "macos", target_os = "ios"))]
-    const TUN_PROTO_IP4: [u8; PIL] = (libc::AF_INET as u32).to_be_bytes();
+    const TUN_PROTO_IP4: [u8; PIL] = (0x0002 as u32).to_be_bytes();
 
     #[cfg(target_os = "freebsd")]
-    const TUN_PROTO_IP6: [u8; PIL] = (libc::AF_INET6 as u32).to_be_bytes();
+    const TUN_PROTO_IP6: [u8; PIL] = (0x86DD as u32).to_be_bytes();
     #[cfg(target_os = "freebsd")]
-    const TUN_PROTO_IP4: [u8; PIL] = (libc::AF_INET as u32).to_be_bytes();
+    const TUN_PROTO_IP4: [u8; PIL] = (0x0800 as u32).to_be_bytes();
 
     #[cfg(unix)]
     if _packet_information {
